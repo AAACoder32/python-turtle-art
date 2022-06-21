7ad87b23d1fcc5814f4a143f8974139ecdc24e1c
Akash Prajapathi <87011163+Bharat444@users.noreply.github.com>
2022-06-21 22:54:41 +0530

    Art70

---
+++ art70.py
@@ -0,0 +1,39 @@
+
+
+import turtle
+import colorsys
+
+turtle.Screen().bgcolor("black")
+
+t = turtle.Pen()
+t.speed(12)
+
+h = 0
+n = 36
+t.penup()
+t.goto(400,0)
+t.pendown()
+
+t.penup()
+t.left(119)
+t.forward(350)
+t.pendown()
+    
+k = 250
+
+while(k!=0):
+    c = colorsys.hsv_to_rgb(h,0.6,0.8)
+    t.pencolor(c)
+    t.fillcolor(c)
+    t.left(49)
+    t.pu()
+    t.forward(k)
+    t.pd()
+    t.begin_fill()
+    t.circle(k,50)
+    t.end_fill()
+        
+    k -= 2
+    h += 5/n
+
+turtle.done()
