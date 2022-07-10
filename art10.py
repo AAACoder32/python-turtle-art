e99dbabfa862ab9ca7ca87a591b4cd8fc147f60e
Akash Prajapathi <87011163+Bharat444@users.noreply.github.com>
2022-07-10 20:07:44 +0530

    Error free

--- art10.py
+++ art10.py
@@ -4,16 +4,13 @@ import turtle
 
 cl = ['#9400d3','#0000ff','#00ff00','#ffff00','#ff7f00','#ff0000']
 
-
-s = turtle.getscreen()
-s.bgcolor('#0b091a')
+turtle.Screen().bgcolor('#0b091a')
 
 t = turtle.Turtle()
 t.pensize(3)
 t.speed(11)
 
 for i in range(100):
-    turtle.speed(10)
     t.pencolor(cl[i%6])
     t.left(59)
     t.forward(150)
