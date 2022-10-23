4178347102ce85b3fcfa3e1d543afecc21cee020
Akash Prajapathi <87011163+AAACoder32@users.noreply.github.com>
2022-10-23 20:02:18 +0530

    Art208

---
+++ art208.py
@@ -0,0 +1,31 @@
+
+# AAACoder Art-208
+import turtle as tu
+import colorsys
+
+tu.Screen().bgcolor("black")
+t = tu.Turtle()
+t.speed(0)
+h = 0.6
+t.pensize(3)
+
+def design(ang,n):
+    t.circle(60+n,90)
+    t.left(ang)
+    t.circle(60+n,90)
+
+for i in range(80):
+    c = colorsys.hsv_to_rgb(h,0.8,1)
+    t.pencolor(c)
+    
+    design(60,i)
+    design(90,i)
+    design(120,i)
+    design(90,i)
+    design(60,i)
+    
+    h += 0.006
+    
+t.ht()
+
+tu.done()
